6c0bff583bb3cb5704197732abd3fddbb7fd68d3
Marlon <marlonkenny@gmail.com>
2014-10-01 17:39:06 +0000

    added new images

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -3,7 +3,7 @@ class Admin::UsersController < ApplicationController
   before_action :only_admin
   
   def index
-    @users = User.all.page(params[:page]).per(10)
+    @users = User.all.page(params[:page])
   end
 
   def new
