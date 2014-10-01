33f029db83f6ade9883f3febbd26acd0e538992c
Marlon <marlonkenny@gmail.com>
2014-10-01 20:25:44 +0000

    duration search working

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -18,17 +18,33 @@ class Movie < ActiveRecord::Base
 
   def self.search(search)
     if search
-      title_search(search['title']).director_search(search['director'])
+      title_search(search['title']).director_search(search['director']).duration(search['duration'])
     else
       all
     end
   end
-  
+
+  scope :runtime_over, ->(time) { where('runtime_in_minutes >= ?', time) }
+  scope :runtime_under, ->(time) { where('runtime_in_minutes <= ?', time) }
+
   def self.title_search(title)
     where('title LIKE ?', "%#{title}%")
   end
 
   def self.director_search(director)
     where('director LIKE ?', "%#{director}%")
+  end
+
+  def self.duration(duration)
+    case duration
+    when 'any'
+      all
+    when '90'
+      runtime_under(90)
+    when '90_120'
+      runtime_over(90).runtime_under(120)
+    when '120'
+      runtime_over(120)
+    end
   end
 end
